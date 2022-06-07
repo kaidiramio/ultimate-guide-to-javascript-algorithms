dc079c6b5fedb1724cc9aba5f6f27e9c3f8bcc61
Kai DiRamio <kaidiramio@gmail.com>
2022-06-06 23:30:06 -0400

    solution

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -4,13 +4,20 @@ return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
 */
 
+function capSentence(text){
+  // cap sentence is a function
+  // turns word into array (characters of word are index easier to target)
+  const allCap = text.toLowerCase().split(' ')
 
+  // new empty array  
+  let capsArr = []
 
+  // 
+  allCap.forEach(function(capWord){
+    capsArr.push(capWord[0].toUpperCase() + capWord.slice(1))
+  })
 
-function capSentence(text) {
-   // Code goes here
+  return capsArr.join(' ')
 }
-
-
 
 module.exports = capSentence
